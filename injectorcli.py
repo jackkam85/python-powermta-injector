24aa688338bd04de39567689ffd54f7dab56017a
epicheals <william.betts@gmail.com>
2012-07-29 23:06:27 -0700

    Added option -s / --hostname sets the hostname / ip of the pmta server to inject into. This option defaults to 127.0.0.1.
    Added option -p / --port sets the port to connect to on the pmta server.
    This option defaults to port 25.

--- injectorcli.py
+++ injectorcli.py
@@ -14,4 +14,6 @@ class InjectorCLI(object):
         parser = argparse.ArgumentParser(description = 'PowerMTA Injector')
         parser.add_argument('-l', '--list', required = True, help = 'The email list to inject', metavar = 'email_list.txt')
         parser.add_argument('-c', '--campaign', required = True, help = 'The campaign to send', metavar = 'example-campaign')
+        parser.add_argument('-s', '--hostname', required = False, help = 'The hostname or IP address of the PMTA server to inject to. If this option is omited it defaults to 127.0.0.1', metavar = '127.0.0.1')
+        parser.add_argument('-p', '--port', required = False, help = 'The port the PMTA server is listening on. If this option is omited it defaults to 25', metavar = '25')
         self.args = parser.parse_args()
