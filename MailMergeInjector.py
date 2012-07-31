c18d9672caf4612e1e2774e2e8835028971189d8
epicheals <william.betts@gmail.com>
2012-07-30 20:18:00 -0700

    The class that does the actual injection

---
+++ MailMergeInjector.py
@@ -0,0 +1,6 @@
+from port25.submitter.message import Message, PmtaMessageError
+from port25.submitter.recipient import Recipient, PmtaRecipientError
+from port25.submitter.connection import Connection, PmtaConnectionError
+from port25 import PmtaError
+
+class MailMergeInjector.py
