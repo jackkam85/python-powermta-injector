c95a2c2858ad0ed5dd2db1171a325b4df12fb9a1
epicheals <william.betts@gmail.com>
2012-08-02 01:56:19 -0700

    added license and header information

--- campaigns.py
+++ campaigns.py
@@ -1,17 +1,41 @@
 """
-This class handles loading the campaigns.
+Copyright (c) 2012, William Betts
+All rights reserved.
 
-Comment:
-    Yes, this is a simple class and probably won't
-    change much, but I don't want to shove this in
-    another class such as the actual injector, because
-    of possible code duplication. I plan on having 
-    different injectors that do different things. 
-    For example one that supports mail merge over smtp,
-    mail merge via file pickup, custom mail merge over
-    smtp that supports bcc (pmta won't do a true bcc with
-    its built in mail merge and it won't support bcc via
-    file pick up even with BMSTP)
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+3. Redistributions of source code must reproduce the author lines at the top 
+   of the original files. You may add to them (i.e. mention you modified the 
+   file), but you may not subtract from them.
+4. If redistributed in binary form you may not strip the author lines from any
+   files before compiling.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+
+Author: William Betts
+File: campaigns.py
+Date: 8-01-2012
+Comments:
+    This class handles the campaign management of the injector. Processing all of the files
+     retrival and of the information.
 """
 
 from glob import glob
