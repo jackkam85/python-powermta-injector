0feb31beaa2318410d745bf0b9f9bbb3077fd236
epicheals <william.betts@gmail.com>
2012-08-02 00:39:18 -0700

    Added in the template variable rdomain. The injector will either use the domain in the from address or a domain from the domains file based on a cli flag.

--- mailmergesmtp.py
+++ mailmergesmtp.py
@@ -6,16 +6,30 @@ from port25.submitter.connection import Connection, PmtaConnectionError
 from port25 import PmtaError
 
 class MailMergeSMTP(object):
-    def __init__(self, campaign_object, smtp_server_hostname = '127.0.0.1', smtp_server_port = '25'): 
+    def __init__(self, use_from, campaign_object, smtp_server_hostname = '127.0.0.1', smtp_server_port = '25'): 
+
+        if use_from == None:
+            self.__use_from = True
+        else:
+            self.__use_from = False
+
         self.__smtp_server_hostname = smtp_server_hostname
         self.__smtp_server_port = smtp_server_port
         self.__campaign_handler = campaign_object
-
 
     def Inject(self, address):
         try:
             print 'Connecting to %s on port %s' % (self.__smtp_server_hostname, self.__smtp_server_port)
             from_address = self.__campaign_handler.GetFrom()
+
+            if self.__use_from == True:
+                from_parts = from_address.split('@') 
+                redirect_domain = from_parts[1]
+            else:
+                redirect_domain = self.__campaign_handler.GetDomain()
+
+                if redirect_domain == None:
+                    raise ValueError('The domains file can not be empty')
 
             rcpt = Recipient(address)
             rcpt.defineVariable('to', address)
@@ -24,6 +38,7 @@ class MailMergeSMTP(object):
             rcpt.defineVariable('subject', self.__campaign_handler.GetSubject())
             rcpt.defineVariable('date', time.strftime("%a, %d %b %Y %H:%M:%S %z", time.gmtime()))
             rcpt.defineVariable('*parts', '1')
+            rcpt.defineVariable('rdomain', redirect_domain)
 
             email = Message(from_address)
 
@@ -36,6 +51,3 @@ class MailMergeSMTP(object):
         except PmtaError as e:
             print 'Connecting to %s on port %s' % (self.__smtp_server_hostname, self.__smtp_server_port)
             print e
-
-        except Exception as e:
-            print e.args
